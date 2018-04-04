d4a6c4ec6d4a0a10c51567b880575f78029bdb94
scrappyzhang <a7478317@163.com>
2018-04-04 11:53:21 +0800

    增加登录验证装饰器common.py  login_required

--- app/utils/common.py
+++ app/utils/common.py
@@ -6,6 +6,10 @@
 '''
 
 from werkzeug.routing import BaseConverter
+import functools
+
+from flask import session, jsonify, g
+from app.utils.response_code import RET
 
 
 class RegexConverter(BaseConverter):
@@ -15,3 +19,27 @@ class RegexConverter(BaseConverter):
         super(RegexConverter, self).__init__(url_map)
 
         self.regex = args[0]
+
+
+# 登录验证装饰器
+def login_required(f):
+    """
+    用户是否登录判断装饰器
+    :param f: 其装饰的函数
+    :return: 装饰器
+    """
+
+    @functools.wraps(f)
+    def wrapper(*args, **kwargs):
+        # 从session中获取当前用户id
+        user_id = session.get("user_id")
+
+        # 如果user_id为None代表用户没有登录,直接返回未登录信息
+        if user_id is None:
+            return jsonify(errno=RET.SESSIONERR, errmsg="用户未登录")
+        else:
+            # 否则使用全局g变量记录当前用户id，并调用被装饰的函数
+            g.user_id = user_id
+            return f(*args, **kwargs)
+
+    return wrapper
--- app/utils/commons.py
+++
@@ -1,6 +0,0 @@
-"""please input file function"""
-'''
-@Time    : 2018/4/4 上午11:37
-@Author  : scrappy_zhang
-@File    : commons.py
-'''
